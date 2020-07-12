e613bf8ed3c42dab4ab5378c54eb80a15d2bce6d
duwaar <duwaar@yahoo.com>
2020-07-12 16:11:26 -0700

    Implemented newline on pressing "Enter" key

--- boot.asm
+++ boot.asm
@@ -5,12 +5,23 @@ jmp 0x0000:start
 start:
     mov si, hello_string
     call print_string
-    mov si, prompt_string
-    call print_string
     prompt:
-        call get_keystroke
-        call print_character
-        jmp prompt
+        mov si, newline_string
+        call print_string
+        mov si, prompt_string
+        call print_string
+        get_command:
+            call get_keystroke
+
+            ; Check if the character typed is "Enter"
+            cmp ah, 0x1c
+            je submit_command
+
+            ; If the typed character was regular ASCII, print it
+            call print_character
+            jmp get_command
+        submit_command:
+            jmp prompt
 
 get_keystroke: ; When a key is pressed, the ASCII character is stored in "al".
     mov ah, 0x00
@@ -35,8 +46,9 @@ print_string: ; Print the characters in the string that "si" points to.
     exit_function:
         ret
 
+newline_string db 0x0d, 0x0a, 0
 input_string db '', 0
 prompt_string db '$ ', 0
-hello_string db 'Hello, World!', 0x0d, 0x0a, 0
+hello_string db 'Hello, World!', 0
 times 510 - ($ - $$) db 0   ; Fill the rest of the sector with "00".
 dw 0xAA55   ; Add boot signature at the end.
